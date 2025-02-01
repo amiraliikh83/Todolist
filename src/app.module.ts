75f696b7cc1d03c80c5a404b527d78308a7ba712
amiraliikh83 <amiraliikh83@gmail.com>
2025-02-01 20:23:20 +0330

    added handler file for handle CRUD item in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { RegisterModule } from './register/register.module';
+import { AuthModule } from './Auth/auth.module';
+import { RegisterModule } from './Register/register.module';
+import { HandlerModule } from './handler/handler.module';
 
 @Module({
-  imports: [AuthModule, RegisterModule],
+  imports: [AuthModule, RegisterModule, HandlerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/handler/handler.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('handler')
+export class HandlerController {}
