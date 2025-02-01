3e4459ed6b407463042c18ef2fdebed63b8181cb
amiraliikh83 <amiraliikh83@gmail.com>
2025-02-01 22:06:05 +0330

    install package

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './Auth/auth.module';
-import { RegisterModule } from './Register/register.module';
 import { HandlerModule } from './Main/Handler/handler.module';
 
 @Module({
-  imports: [AuthModule, RegisterModule, HandlerModule],
+  imports: [AuthModule, HandlerModule],
   controllers: [AppController],
   providers: [AppService],
 })
