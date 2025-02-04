3f7e2217243fc601140a24e14d3901637ded70ed
amiraliikh83 <amiraliikh83@gmail.com>
2025-02-04 14:27:37 +0330

    added JwtService ot auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './Auth/auth.module';
 import { HandlerModule } from './Main/Handler/handler.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, HandlerModule],
+  imports: [AuthModule, HandlerModule, JwtService],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [JwtService],
+  providers: [AuthService, JwtService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
