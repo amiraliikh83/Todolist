7f1389c70c3c17685346b28bad2212f9dd12749c
amiraliikh83 <amiraliikh83@gmail.com>
2025-02-14 00:23:17 +0330

    create file for checking and type item Crud Todo

---
+++ src/DTO/CrudTodo.Dto.ts
@@ -0,0 +1,20 @@
+import { IsArray, IsBoolean, IsNumber, IsString } from 'class-validator';
+export class CrudTodo {
+  @IsString()
+  TodoSubject: string;
+
+  @IsNumber()
+  TodoFolder: number;
+
+  @IsNumber()
+  TodoPriority: number;
+
+  @IsBoolean()
+  IsListTodo: boolean;
+
+  @IsString()
+  TodoDescription: string;
+
+  @IsArray()
+  TodoItem: string[];
+}
